df2ea710786acd1ea244b51eea511c32e3a70a95
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2024-10-11 00:03:09 +0900

    :sparkles: example code for React.memo

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -1,6 +1,9 @@
+import React from 'react';
 import { useState, useEffect } from 'react';
 
 export function Timer() {
+  console.log('Timer component rendered');
+
   const [time, setTime] = useState(0);
   const [paused, setPaused] = useState(false);
 
@@ -18,10 +21,32 @@ export function Timer() {
     setPaused((prevPaused) => !prevPaused);
   }
 
+  const handleClick2 = () => {
+    console.log('clicked');
+  }
+
   return (
     <>
       <p>time: { time }</p>
       <button type='button' onClick={handleClick}>pause</button>
+      <Child />
+      <Child2 handleClick2={handleClick2} />
     </>
   )
 }
+
+const Child = React.memo(() => {
+  console.log('Child component rendered');
+
+  return (
+    <p>Child component</p>
+  )
+});
+
+const Child2 = React.memo((props) => {
+  console.log('Child2 component rendered');
+
+  return (
+    <button onClick={props.handleClick2} type='button'>Child</button>
+  )
+})
