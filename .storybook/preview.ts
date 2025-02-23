97bb330126c779a5ecb89f704210cc57e6ef2a06
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2025-02-23 14:36:45 +0900

    :bug: change mockServiceWorker.js path

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,12 +2,17 @@ import type { Preview } from "@storybook/react";
 import { initialize, mswLoader } from 'msw-storybook-addon';
 import { handlers } from './../mocks/handers';
 
+let options = {};
+if (location.hostname === "username.github.io") {
+  options = {
+    serviceWorker: {
+      url: "/repo-name/mockServiceWorker.js",
+    },
+  };
+}
+
 // Initialize MSW
-initialize({
-  serviceWorker: {
-    url: '/mockServiceWorker.js',
-  }
-});
+initialize(options);
 
 const preview: Preview = {
   parameters: {
