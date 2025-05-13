d14d2502ed3782d77b4d05fcbba1e37c763c628b
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2025-05-13 21:11:12 +0900

    chore: update Storybook dependencies and migrate to react-vite
    - Upgraded @chromatic-com/storybook to version 4.0.0-next.6
    - Updated Storybook addons to version 9.0.0-rc.0
    - Changed import statements in story files from '@storybook/react' to '@storybook/react-vite' for compatibility with the new version

--- src/components/counter/index.stories.tsx
+++ src/components/counter/index.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react-vite';
 import { Counter } from './index';
 
 const meta: Meta<typeof Counter> = {
